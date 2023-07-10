ea91c214ac9f3dc99a4db11abdcead08435a4da1
Gabriel Silva M. G <gsilvexist7@gmail.com>
2023-07-10 19:19:38 -0400

    home base and explorar base

--- Platform/urls.py
+++ Platform/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('explorar_eventos/', views.explorar_eventos, name='explorar_eventos')
 ]
--- Platform/views.py
+++ Platform/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def explorar_eventos(request):
+    return render(request, 'explorar_eventos.html')
