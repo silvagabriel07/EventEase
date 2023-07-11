c2782adff28b0182d1c80c2cb9b66a5473655334
Gabriel Silva M. G <gsilvexist7@gmail.com>
2023-07-11 16:52:56 -0400

    before Event model migration

--- eventos/models.py
+++ eventos/models.py
@@ -1,17 +1,26 @@
 from django.db import models
-
+from account_manager.models import User
 # Create your models here.
 
-class EventCategory(models.Model):
-    name = models.CharField(max_length=40)
-    
 class Event(models.Model):
     title = models.CharField(max_length=40)
     description = models.TextField()
-    category = models.ManyToManyField('EventCategory')
-    free = models.BooleanField()
-    start_time = models.DateTimeField()
-    start_date = models.DateField()
-    final_date = models.DateField()
-    final_time = models.DateTimeField()
 
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(User)
+
+    CATEGORIES = (
+        ('birthday', 'Aniversário'),
+        ('category_a', 'Categoria A'),
+        ('category_b', 'Categoria B'),
+        ('category_c', 'Categoria C'),
+    )
+
+    category = models.CharField(max_length=15, choices=CATEGORIES)
+    private = models.BooleanField(help_text='definir como True torna preciso a aceitação da requisição de participação')
+    free = models.BooleanField(help_text='definir como true significa que é um evento livre, sem restrição de idade')
+    start_date_time = models.DateTimeField()
+    final_date_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
