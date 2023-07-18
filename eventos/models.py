12fca6c8f0eb6fa1553bc32b3d38eaf8c0e57c76
Gabriel Silva M. G <gsilvexist7@gmail.com>
2023-07-17 21:47:09 -0400

    'Request' model (before migrations)

--- eventos/models.py
+++ eventos/models.py
@@ -17,6 +17,8 @@ class Event(models.Model):
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
     participants = models.ManyToManyField(User, related_name='event_participants', blank=True)
 
+    requests = models.ManyToManyField(User, related_name='user_requests', blank=True)
+
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)  
     private = models.BooleanField(help_text='definir como True torna preciso a aceitação da requisição de participação')
     free = models.BooleanField(help_text='definir como True significa que é um evento livre, sem restrição de idade')
@@ -29,3 +31,17 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Request(models.Model):
+    STATUS = (
+        ('a', 'Aceito'),
+        ('w', 'Aguardando'),
+        ('r', 'Rejeitado')
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=STATUS, default='w')
+
+    class Meta:
+        unique_together = ('user', 'event')
