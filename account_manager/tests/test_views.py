6f442454d8555a5646bc3a58f3e9046730c6fb69
Gabriel Silva M. G <gsilvexist7@gmail.com>
2023-09-27 21:54:32 -0400

    added account_manager.test_views.TestViewCustomSignupView

--- account_manager/tests/test_views.py
+++ account_manager/tests/test_views.py
@@ -24,3 +24,48 @@ class TestViewAccountInactive(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
+class TestViewCustomSignupView(TestCase):
+    
+    def setUp(self) -> None:
+        self.url = reverse('account_signup')
+        
+    def test_get_signup_return_OK_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_post_signup_with_valid_form(self):
+        response = self.client.post(self.url, data={
+            'username': 'user',
+            'idade': 17,
+            'email': 'email@gmail.com',
+            'password1': 'senhaqualquer12',
+            'password2': 'senhaqualquer12',
+        })
+        self.assertRedirects(response, reverse('account_inactive'))
+        self.assertEqual(User.objects.all().count(), 1)
+
+    @patch('account_manager.views.activateEmail', autospec=True)
+    def test_post_signup_with_valid_form_call_activate_Email_function(self, mock_activateEmail):
+        response = self.client.post(self.url, data={
+            'username': 'user',
+            'idade': 17,
+            'email': 'email@gmail.com',
+            'password1': 'senhaqualquer12',
+            'password2': 'senhaqualquer12',
+        })
+        mock_activateEmail.assert_called_once()
+
+    def test_post_signup_with_invalid_form_redirects_to_signup_with_form_errors(self):
+        response = self.client.post(self.url, data={
+            'username': 'user',
+            'idade': 17,
+            'email': 'email@gmail.com',
+            'password1': '00000000',
+            'password2': 'senhaqualquer12',
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.context.get('form'))
+        self.assertGreaterEqual(len(response.context.get('form').errors), 1)
+    
+
