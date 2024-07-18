f67219c379e1286ad8eddfa2fd25bb3f4ea5fdfd
Gabriel Silva M. G. <gsilvexist7@gmail.com>
2024-07-17 23:17:18 -0400

    whitenoise on wsgi.py

--- core/settings.py
+++ core/settings.py
@@ -188,7 +188,7 @@ DJANGO_NOTIFICATIONS_CONFIG = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static'),
 ]
